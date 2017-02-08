e2e1d26a58430583ce9c3519dfe6ab61d7b860aa
Elliott Geier <geier.elliott@gmail.com>
2017-02-08 14:11:49 +0100

    Created models

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  resources :pictures
+  resources :photo_booths
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
